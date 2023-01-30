5f2185a396c959f5933fe852bf36fcfa769f09cd
SubjectZero0 <iordanoualexander@gmail.com>
2023-01-30 17:00:59 +0200

    creates pydantic post model

--- todos_api/models.py
+++ todos_api/models.py
@@ -1,28 +1,23 @@
 from sqlalchemy import Boolean, Column, Integer, String
+from pydantic import BaseModel, Field
+
+from typing import Optional
 from database.db import Base
+
 
 class Todos(Base):
     """Model-table for Todos"""
     __tablename__ = 'todos'
-    
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    complete = Column(Boolean)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    complete = Column(Boolean, default=False)
+
+
+class PostTodo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
