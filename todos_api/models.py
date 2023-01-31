328b672f73da8548b805a8fd87971000b5b9abd0
SubjectZero0 <iordanoualexander@gmail.com>
2023-01-31 12:43:37 +0200

    Creates user model and relationship(1-m) to todos

--- todos_api/models.py
+++ todos_api/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import Relationship
 from pydantic import BaseModel, Field
 
 from typing import Optional
@@ -14,6 +15,9 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = Relationship('Users', back_populates="todos")
 
 
 class PostTodo(BaseModel):
@@ -22,3 +26,24 @@ class PostTodo(BaseModel):
     description: Optional[str]
     priority: int = Field(gt=0, lt=6)
     complete: bool
+
+# --------------------------------------------------------------
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+
+    todos = Relationship('Todos', back_populates='owner')
+
+
+class CreateUser(BaseModel):
+    username: str
